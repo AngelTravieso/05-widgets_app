0a5ac42e0106c079e3f78a9bee86a3e9022b5877
atravieso <angeltraviesoc@gmail.com>
2023-03-30 11:33:00 -0400

    screens creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
 export 'buttons/buttons_screen.dart';
 export 'cards/cards_screen.dart';
 export 'home/home_screen.dart';
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/inifinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';

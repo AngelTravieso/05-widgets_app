3b65a8d6f931e499db5bbc2b4db561d6f68d1107
AngelTravieso <angeltraviesoc@gmail.com>
2023-10-25 15:52:51 -0400

    Consideraciones del notch y opciones del menu

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -14,22 +14,43 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+    // Indica cuanto es el padding de alguna direccion
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
-      selectedIndex: navDrawerIndex,
+      selectedIndex: navDrawerIndex, //
       onDestinationSelected: (value) {
         setState(() {
           navDrawerIndex = value;
         });
       },
       children: [
-        NavigationDrawerDestination(
-          icon: Icon(Icons.add),
-          label: const Text('Home Screen'),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, hasNotch ? 10 : 20, 16, 10),
+          child: const Text('Main'),
         ),
-        NavigationDrawerDestination(
-          icon: Icon(Icons.add_shopping_cart),
-          label: const Text('Otra pantalla'),
+        // Obtener primeros 3 elementos del menú
+        ...appMenuItems.sublist(0, 3).map(
+              (item) => NavigationDrawerDestination(
+                icon: Icon(item.icon),
+                label: Text(item.title),
+              ),
+            ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
         ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+          child: Text('More options'),
+        ),
+        // Obtener resto de los elementos del menú
+        ...appMenuItems.sublist(3).map(
+              (item) => NavigationDrawerDestination(
+                icon: Icon(item.icon),
+                label: Text(item.title),
+              ),
+            ),
       ],
     );
   }
